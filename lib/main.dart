334cd3f1db9db6ca5136cf89bd52300cb29cdc52
Lunapettersson <Lpettersson95@gmail.com>
2021-11-19 15:16:57 +0100

    Nya appen

--- lib/main.dart
+++ lib/main.dart
@@ -1,134 +1,34 @@
 import 'package:flutter/material.dart';
-import 'secondview.dart';
+import './model.dart';
+import 'main_view.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(LukasApp());
+  var state = MyState();
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => state,
+      child: MyApp(),
+    ),
+  );
 }
 
-class LukasApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter demo',
+      theme: ThemeData(
+          primarySwatch: Colors.blueGrey,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+          dividerTheme: DividerThemeData(
+              space: 50,
+              thickness: 10,
+              color: Colors.grey,
+              indent: 20,
+              endIndent: 20)),
       home: MainView(),
     );
-  }
-}
-
-class MainView extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: Text('TIG169: Att göra'),
-        actions: [
-          PopupMenuButton(
-              itemBuilder: (context) => [
-                    PopupMenuItem(
-                      child: Text("All"),
-                      value: 1,
-                    ),
-                    PopupMenuItem(
-                      child: Text("Done"),
-                      value: 2,
-                    ),
-                    PopupMenuItem(
-                      child: Text("Undone"),
-                      value: 3,
-                    )
-                  ])
-        ],
-      ),
-      body: _list(),
-      floatingActionButton: FloatingActionButton(
-          backgroundColor: Colors.blue,
-          child: Icon(
-            Icons.add,
-          ),
-          onPressed: () {
-            Navigator.push(
-                context, MaterialPageRoute(builder: (context) => SecondView()));
-          }),
-    );
-  }
-
-  Widget _list() {
-    var assignment = [
-      "Gör gröt",
-      "Läxa",
-      "Tänka",
-      "Leka",
-      "Träna",
-      "Sova",
-      "Jobba",
-    ];
-    return ListView.builder(
-        itemCount: assignment.length,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              _listItems(assignment[index]),
-              Divider(
-                thickness: 2,
-              ),
-            ],
-          );
-        });
-  }
-
-  Widget _listItems(assignmentTitle) {
-    return ListTile(
-      leading: MyCB(),
-      title: Text(
-        assignmentTitle,
-        style: TextStyle(
-          fontSize: 20,
-        ),
-      ),
-      trailing: IconButton(
-        icon: Icon(Icons.delete),
-        onPressed: () {},
-      ),
-    );
-  }
-
-  Widget _checkBoxAlt(String assignment) {
-    return Row(children: [
-      MyCB(),
-      Container(
-        height: 30,
-        width: 250,
-        child: Text(
-          assignment,
-          style: const TextStyle(
-            fontSize: 25,
-          ),
-        ),
-      ),
-      IconButton(
-        icon: Icon(Icons.delete),
-        onPressed: () {},
-      )
-    ]);
-  }
-}
-
-class MyCB extends StatefulWidget {
-  @override
-  State<MyCB> createState() => _MyCBState();
-}
-
-class _MyCBState extends State<MyCB> {
-  bool? _myBool = false;
-
-  @override
-  Widget build(BuildContext context) {
-    return Checkbox(
-        value: _myBool,
-        onChanged: (value) {
-          setState(() {
-            _myBool = value;
-          });
-        });
   }
 }
